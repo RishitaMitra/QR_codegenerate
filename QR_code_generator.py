50c2b2fe381456de1b4537f2119e36c41b62c6fb
Rishita <85801090+RishitaMitra@users.noreply.github.com>
2022-12-29 13:57:23 +0530

    Add files via upload

---
+++ QR_code_generator.py
@@ -0,0 +1,13 @@
+#QR code generate for any link with 
+import qrcode
+#from PIL import Image
+#img = qr.make("https://www.linkedin.com/in/rishita-mitra-242716226")
+
+#img.save("Rishita_LinkedIn.png")
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=5)
+qr.add_data("https://www.linkedin.com/in/rishita-mitra-242716226")
+qr.make(fit=True)
+img=qr.make_image(fill_color="blue", back_color="black")
+img.save("linkdin_Rishita.png")
